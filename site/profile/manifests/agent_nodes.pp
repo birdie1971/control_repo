254fbf99bf3d02c6f524a1daae0922244641ada6
birdie1971 <70715583+birdie1971@users.noreply.github.com>
2021-07-07 13:07:09 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'mindtest.puppet.vm':}
   host {'web.puppet.vm':
     ensure => present,
     ip     => '172.10.0.2',
